c2249d7c070ac05c9091c25225233495dca16beb
RandomGuy090 <64653975+RandomGuy090@users.noreply.github.com>
2020-07-05 23:40:08 +0200

    added info about importing and installing packages

--- MemeZfinder.py
+++ MemeZfinder.py
@@ -13,25 +13,29 @@ elif "windows" in osType.lower():
 
 
 try:
+	print("import PyQt5")
 	from PyQt5.Qt import Qt 
 	from PyQt5.QtWidgets import *
 	import pyautogui
 except:
+	print("installing PyQt5")
 	print(os.popen("pip3 install PyQt5").read())
 	print(os.popen("pip3 install pyautogui").read())
+
 	
 
 if osType == "windows":
 	try:
+		print("import win32clipboard")
 		import pkg_resources
 		print("pkg_resources")
 		pkg_resources.require("pywin32==227")
 		print("win32clipboard") 
 	except:
+		print("installing win32clipboard")
 		print(os.popen("pip3 install pywin32").read())
 
 from coordFunc import *
-#from windowClasses import * 
 from mainWindow import *
 
 
